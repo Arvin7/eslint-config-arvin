3de3e53dd31a3d78b26b8d6b80415fadf52fd2bf
Arvin <ariashtary@gmail.com>
2023-04-06 13:27:50 +0200

    Add import plugins

--- index.js
+++ index.js
@@ -1,7 +1,36 @@
 module.exports = {
+  extends: ["eslint:recommended", "plugin:import/recommended"],
+
+  plugins: ["import", "unused-imports"],
+
   rules: {
     "prefer-template": "error",
     "prefer-destructuring": "error",
     "object-shorthand": "error",
+
+    "unused-imports/no-unused-imports": "error",
+    "import/no-unused-modules": "error",
+    "import/newline-after-import": "error",
+    "import/order": [
+      "error",
+      {
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: false,
+        },
+        groups: ["builtin", "external", "internal"],
+        "newlines-between": "always",
+      },
+    ],
+
+    "react/jsx-curly-brace-presence": ["error", "never"],
+
+    "@typescript-eslint/naming-convention": [
+      "error",
+      {
+        selector: "enumMember",
+        format: ["UPPER_CASE"],
+      },
+    ],
   },
 };
