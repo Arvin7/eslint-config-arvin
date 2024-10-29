49ae837b5ed5e3fb2544d121f6db41076a6a679a
Arvin <ariashtary@gmail.com>
2024-10-29 09:29:49 +0100

    Fix React Hooks config

--- README.md
+++ README.md
@@ -6,3 +6,17 @@ One plugin to share my common ESLint rules across different projects.
 
 - Use v2.x for ESLint v9.13.0 and above
 - Use v1.0.9 for ESLint v8.57.0 and below
+
+## Usage
+
+1. Create a ESLint config file (should be module file: `mjs`) in your project root directory:
+   `eslint.config.mjs`
+
+2. Copy/paste:
+
+```js
+import arvinConfig from "eslint-config-arvin";
+import arvinReactConfig from "eslint-config-arvin/react.js";
+
+export default [...arvinConfig, ...arvinReactConfig];
+```
--- react.js
+++ react.js
@@ -4,9 +4,12 @@ import reactHooksPlugin from "eslint-plugin-react-hooks";
 export default [
   reactPlugin.configs.flat.recommended,
   reactPlugin.configs.flat["jsx-runtime"],
-  reactHooksPlugin.configs.flat.recommended,
   {
+    plugins: {
+      "react-hooks": reactHooksPlugin,
+    },
     rules: {
+      ...reactHooksPlugin.configs.recommended.rules,
       "react/jsx-curly-brace-presence": ["error", "never"],
       "react/jsx-boolean-value": ["error", "never"],
       "react/self-closing-comp": ["error"],
